04cc68aa95cc993b4882f6e873ff011edbb47f99
Yasushi Komori <yasushi.komori@gii1508besnivel.attlocal.net>
2016-04-10 02:24:47 -0700

    add

---
+++ Frontier/Assets/Editer/VRBuildSettings.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR 
+using UnityEditor;
+
+
+public class VRBuildSettings {
+
+	[MenuItem("VR Build Settings/Build GearVR")]
+	static void BuildGearVRSettings() {
+		PlayerSettings.virtualRealitySupported = true;
+		Cardboard cardboard = (Cardboard)GameObject.FindGameObjectWithTag ("Player").GetComponent<Cardboard> ();
+		CardboardHead cardboardHead = cardboard.gameObject.transform.GetChild (0).GetComponent<CardboardHead>();
+		cardboard.VRModeEnabled = false;
+		cardboardHead.trackPosition = false;
+		cardboardHead.trackRotation = false;
+		EditorUtility.SetDirty (cardboard);
+		EditorUtility.SetDirty (cardboardHead);
+
+	}
+
+	[MenuItem("VR Build Settings/Build Cardboard")]
+	static void BuildCardboardSettings() {
+		PlayerSettings.virtualRealitySupported = false;
+		Cardboard cardboard = (Cardboard)GameObject.FindGameObjectWithTag ("Player").GetComponent<Cardboard> ();
+		CardboardHead cardboardHead = cardboard.gameObject.transform.GetChild (0).GetComponent<CardboardHead>();
+		cardboard.VRModeEnabled = true;
+		cardboardHead.trackPosition = true;
+		cardboardHead.trackRotation = true;
+		EditorUtility.SetDirty (cardboard);
+		EditorUtility.SetDirty (cardboardHead);
+
+	}
+
+}
+
+#endif
